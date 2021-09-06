b6d4e18c8fa52befb37cc0c2c906297e5349a562
KaleoRaul <miguassisjos@gmail.com>
2021-09-06 14:17:11 -0300

    rrrrr
    rrr

--- src/components/AccountSetup.js
+++ src/components/AccountSetup.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import logo from './carrfo.png'
-import InputMask from "react-input-mask";
 
 
 
--- src/components/SocialProfiles.js
+++ src/components/SocialProfiles.js
@@ -1,4 +1,4 @@
-import React, { Component,useState } from 'react'
+import React, { Component } from 'react'
 import logo from './carrfo.png'
 import emailjs from 'emailjs-com';
 
